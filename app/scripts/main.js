ae9c85cb8c3c784564fe5eb72310fb55c8172efa
olga <olgaa.yatsuk@gmail.com>
2018-10-05 12:39:37 +0300

    fixed hero;

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,11 @@
-const burger = document.querySelector('.header__nav-toggler'),
-      body = document.body;
-
-burger.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  body.classList.toggle('nav-open');
-});
+// const burger = document.querySelector('.header__nav-toggler'),
+//       body = document.body;
+//
+// burger.addEventListener('click', (e) => {
+//   e.preventDefault();
+//
+//   body.classList.toggle('nav-open');
+// });
 
 const $mainSlider = $('.js-main-slider');
 const $mediaSlider = $('.js-media');
